f7ff8d5468f2dc1320ff28d733ab7a98957c2fd3
biancavictorino <biancamarie.victorino@gmail.com>
2024-05-27 23:51:13 +0800

    added hamburger

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.header-menu');
+
+    hamburger.addEventListener('click', function() {
+        menu.classList.toggle('show')
+    })
+    
+})
+
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
